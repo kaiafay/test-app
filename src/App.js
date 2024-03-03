94c82ee41132fcbbbb99cf174add75d21ae9cb60
Kaia Scheirman <carls@MacBook-Pro.local>
2024-03-03 14:01:56 -0800

    create footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/index';
+import Footer from './components/footer';
 
 function App() {
   return (
     <Router>
       <div>
         <Header />
+        <Footer />
       </div>
     </Router>
   );
